9085eead69f68a1655a605394e807d4cf69bb30d
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-11-08 15:45:07 +0000

    refactor surveyprogressbar

--- src/components/SurveyProgressBar.js
+++ src/components/SurveyProgressBar.js
@@ -2,36 +2,14 @@ import React from "react";
 import StyledSurveyProgressBar from "./styled/SurveyProgressBar.styled";
 
 const SurveyProgressBar = (props) => {
+    console.log(props);
+    const percents = [2, 4, 6, 7, 8, 9, 10];
+
     const percent = () => {
-        if (props.currStep === 1) {
-            const percent = 2;
-            return percent;
-        }
-        if (props.currStep === 2) {
-            const percent = 4;
-            return percent;
-        }
-        if (props.currStep === 3) {
-            const percent = 6;
-            return percent;
-        }
-        if (props.currStep === 4) {
-            const percent = 7;
-            return percent;
-        }
-        if (props.currStep === 5) {
-            const percent = 8;
-            return percent;
-        }
-        if (props.currStep === 6) {
-            const percent = 9;
-            return percent;
-        }
-        if (props.currStep === 7) {
-            const percent = 10;
-            return percent;
-        } else return;
+        const current = percents[props.currStep - 1];
+        return current;
     };
+
     const percentValue = percent();
     return (
         <StyledSurveyProgressBar percent={percentValue}>
