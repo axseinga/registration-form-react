4d4d33ada0294491e906fe88b7c768f27e43117c
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-11-08 16:17:02 +0000

    refactor surveytabs

--- src/components/SurveyProgressBar.js
+++ src/components/SurveyProgressBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import StyledSurveyProgressBar from "./styled/SurveyProgressBar.styled";
 
 const SurveyProgressBar = (props) => {
-    console.log(props);
     const percents = [2, 4, 6, 7, 8, 9, 10];
 
     const percent = () => {
--- src/components/SurveyTabs.js
+++ src/components/SurveyTabs.js
@@ -3,64 +3,22 @@ import StyledSurveyTabs from "./styled/SurveyTabs.styled";
 import SurveyTabItem from "./SurveyTabItem";
 
 const SurveyTabs = (props) => {
+    const tabs = [1, 2, 3, 4, 5, 6, 7];
     return (
         <StyledSurveyTabs>
-            <SurveyTabItem
-                active={props.currStep === 1 ? true : false}
-                disabled={false}
-                handleChangeStep={props.handleChangeStep}
-                step={1}
-            >
-                Step 1
-            </SurveyTabItem>
-            <SurveyTabItem
-                active={props.currStep === 2 ? true : false}
-                disabled={false}
-                handleChangeStep={props.handleChangeStep}
-                step={2}
-            >
-                Step 2
-            </SurveyTabItem>
-            <SurveyTabItem
-                active={props.currStep === 3 ? true : false}
-                disabled={false}
-                handleChangeStep={props.handleChangeStep}
-                step={3}
-            >
-                Step 3
-            </SurveyTabItem>
-            <SurveyTabItem
-                active={props.currStep === 4 ? true : false}
-                disabled={false}
-                handleChangeStep={props.handleChangeStep}
-                step={4}
-            >
-                Step 4
-            </SurveyTabItem>
-            <SurveyTabItem
-                active={props.currStep === 5 ? true : false}
-                disabled={false}
-                handleChangeStep={props.handleChangeStep}
-                step={5}
-            >
-                Step 5
-            </SurveyTabItem>
-            <SurveyTabItem
-                active={props.currStep === 6 ? true : false}
-                disabled={false}
-                handleChangeStep={props.handleChangeStep}
-                step={6}
-            >
-                Step 6
-            </SurveyTabItem>
-            <SurveyTabItem
-                active={props.currStep === 7 ? true : false}
-                disabled={false}
-                handleChangeStep={props.handleChangeStep}
-                step={7}
-            >
-                Step 7
-            </SurveyTabItem>
+            {tabs.map((tab, index) => {
+                const tabNum = index + 1;
+                return (
+                    <SurveyTabItem
+                        active={props.currStep === tabNum ? true : false}
+                        disabled={false}
+                        handleChangeStep={props.handleChangeStep}
+                        step={tabNum}
+                    >
+                        Step {tabNum}
+                    </SurveyTabItem>
+                );
+            })}
         </StyledSurveyTabs>
     );
 };
